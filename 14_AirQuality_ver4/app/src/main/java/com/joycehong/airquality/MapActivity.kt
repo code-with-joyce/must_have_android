a593efb4c59aec26e44785fead0400895e8fc6ce
Joyce Hong <joyceHong0524@gmail.com>
2021-11-17 04:10:46 +0900

    upload

---
+++ 14_AirQuality_ver4/app/src/main/java/com/joycehong/airquality/MapActivity.kt
@@ -0,0 +1,85 @@
+package com.joycehong.airquality
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.joycehong.airquality.databinding.ActivityMapBinding
+
+class MapActivity : AppCompatActivity() ,OnMapReadyCallback{
+
+    lateinit var binding: ActivityMapBinding
+
+    private var mMap: GoogleMap? = null
+    var currentLat: Double = 0.0 //MainActivity.kt에서 전달된 위도
+    var currentLng: Double = 0.0 //MainActivity.kt에서 전달된 경도
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //MainActivity.kt 에서 intent로 전달된 값을 가져옵니다.
+        currentLat = intent.getDoubleExtra("currentLat", 0.0)
+        currentLng = intent.getDoubleExtra("currentLng", 0.0)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
+        binding.btnCheckHere.setOnClickListener {
+            mMap?.let {
+                val intent = Intent()
+                intent.putExtra("latitude", it.cameraPosition.target.latitude)
+                intent.putExtra("longitude", it.cameraPosition.target.longitude)
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+        }
+    }
+
+    //지도가 준비되었을 때 실행되는 콜백
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mMap?.let{
+            val currentLocation = LatLng(currentLat, currentLng)
+            it.setMaxZoomPreference(20.0f) //줌 최대값 설정
+            it.setMinZoomPreference(12.0f) //줌 최솟값 설정
+            it.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16f))
+        }
+
+        setMarker()
+
+        binding.fabCurrentLocation.setOnClickListener {
+            val locationProvider = LocationProvider(this@MapActivity)
+            //위도와 경도 정보를 가져옵니다.
+            val latitude = locationProvider.getLocationLatitude()
+            val longitude = locationProvider.getLocationLongitude()
+            mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude, longitude), 16f))
+            setMarker()
+        }
+    }
+
+    //마커 설정하는 함수
+    private fun setMarker(){
+        mMap?.let{
+            it.clear() //지도에 있는 마커를 먼저 삭제
+            val markerOptions = MarkerOptions()
+            markerOptions.position(it.cameraPosition.target) //마커의 위치 설정
+            markerOptions.title("마커 위치") //마커의 이름 설정
+            val marker = it.addMarker(markerOptions) //지도에 마커를 추가하고, 마커 객체를 리턴
+            it.setOnCameraMoveListener {
+                marker?.let { marker ->
+                    marker.setPosition(it.cameraPosition.target)
+                }
+            }
+        }
+    }
+
+}
---
+++ 14_AirQuality_ver4/app/src/main/java/com/joycehong/airquality/retrofit/AirQualityService.kt
@@ -0,0 +1,18 @@
+package com.joycehong.airquality.retrofit
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * @author Joyce Hong
+ * @email joycehong0524@gmail.com
+ * @created 2021/09/03
+ * @desc
+ */
+interface AirQualityService {
+
+    @GET("nearest_city")
+    fun getAirQualityData(@Query("lat") lat : String, @Query("lon") lon : String, @Query("key") key : String ) : Call<AirQualityResponse>
+
+}
