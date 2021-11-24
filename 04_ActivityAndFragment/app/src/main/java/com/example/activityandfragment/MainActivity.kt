9292e0c1b546e60a38429fc4d174c4127dd50f15
Joyce Hong <joyceHong0524@gmail.com>
2021-11-24 17:31:02 +0900

    Update

--- 04_ActivityAndFragment/app/src/main/java/com/example/activityandfragment/MainActivity.kt
+++ 04_ActivityAndFragment/app/src/main/java/com/example/activityandfragment/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.activityandfragment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        settingButton() // ❹
+    }
+
+    fun settingButton(){
+        val button = findViewById<Button>(R.id.button) // ❶
+        button.setOnClickListener {
+            val intent = Intent(this,SubActivity::class.java) // ❷
+            startActivity(intent) // ❸
+        }
     }
 }
